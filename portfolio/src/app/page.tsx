71d4867e28e75b59b0a398ae0369843dcc95d3c7
Akhil Sai Koppanatham <akoppanatham@adaptivebiotech.com>
2024-08-02 01:03:06 -0400

    added icon

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -7,6 +7,7 @@ import Hero from "./sections/hero";
 import Coming from "./sections/coming";
 import Linkedin from "./sections/linkedin";
 
+
 export default function Home() {
   return (
     <main>
